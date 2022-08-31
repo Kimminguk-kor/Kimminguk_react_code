577590eba99de57437995b31f83bb93f14551441
gun2dal@gmail.com <gun2dal@gmail.com>
2022-08-31 14:11:28 +0900

    0831update_responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,14 @@ const Home = () => {
     <div className="innerbox">
       <div className="container homeBox">
         <div className="titleBox">
-          {/* <div className="homeImg"><img src="../images/about.png"/></div> */}
           <div>
             <p>Portfolio</p>
-            <p>Publisher</p>
+            <p className="middleText">Publisher</p>
             <p className="maskText">
               <span className="mask">JavaScript</span>&amp;
               <span className="mask">HTML / CSS</span>
             </p>
-            <p>Studying React Dev</p>
+            <p className="middleText">Studying React Dev</p>
           </div>
         </div>
       </div>
